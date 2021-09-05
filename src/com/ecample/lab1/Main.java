5f97d9672fe0413c22de69511ee3adaf5b435b35
21901019 <akqlshrl2002@naver.com>
2021-09-05 20:12:32 +0900

    1st commit

---
+++ src/com/ecample/lab1/Main.java
@@ -0,0 +1,16 @@
+package com.ecample.lab1;
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String [] args) {
+		System.out.println("Hello World!!!\n");
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("이름 입력 : ");
+		String str = s.next();
+		System.out.println("["+str+"]님 환영합니다~");
+		
+	}
+
+}
